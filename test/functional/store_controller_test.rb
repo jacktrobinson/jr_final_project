a52e583379a2f80e2d193a3dac07de8ad8769f83
Jack Robinson <jrobins@wharton.upenn.edu>
2012-12-10 22:07:59 -0500

    Another commit

--- test/functional/store_controller_test.rb
+++ test/functional/store_controller_test.rb
@@ -4,6 +4,9 @@ class StoreControllerTest < ActionController::TestCase
   test "should get index" do
     get :index
     assert_response :success
+    assert_select '#columns #side a'
+    assert_select '#main .entry'
+    assert_select 'h3'
+    assert_select '.price'
   end
-
 end
